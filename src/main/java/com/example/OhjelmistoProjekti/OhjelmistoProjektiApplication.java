52b46b1d04bff2acf18e516ef05501252d3808eb
teemuhirvonen <Teemuh25@gmail.com>
2019-03-25 10:14:39 +0200

    first commit

---
+++ src/main/java/com/example/OhjelmistoProjekti/OhjelmistoProjektiApplication.java
@@ -0,0 +1,13 @@
+package com.example.OhjelmistoProjekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistoProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistoProjektiApplication.class, args);
+	}
+
+}
