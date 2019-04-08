2946c81fa310b7ef5d678a5697815f6156f1a7c9
teemuhirvonen <Teemuh25@gmail.com>
2019-04-08 09:11:51 +0300

    crossOrigin

--- src/main/java/com/example/OhjelmistoProjekti/web/OhjelmistoproController.java
+++ src/main/java/com/example/OhjelmistoProjekti/web/OhjelmistoproController.java
@@ -1,11 +1,11 @@
 package com.example.OhjelmistoProjekti.web;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +16,7 @@ import com.example.OhjelmistoProjekti.domain.Question;
 import com.example.OhjelmistoProjekti.domain.QuestionRepository;
 
 
-
+@CrossOrigin
 @Controller
 public class OhjelmistoproController {
 	
